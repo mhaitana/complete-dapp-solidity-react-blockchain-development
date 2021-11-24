e97065e65463da7e645e995200177fce1d6f0421
Matt Haitana <mhaitana@gmail.com>
2021-11-24 20:23:56 +1100

    Section 12

---
+++ Section 9-12/defi-staking-app-starter/defi-staking-app-starter/test/decentralBank.test.js
@@ -0,0 +1,54 @@
+const { assert } = require('chai')
+
+const RWD = artifacts.require('RWD')
+const Tether = artifacts.require('Tether')
+const DecentralBank = artifacts.require('DecentralBank')
+
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('DecentralBank', ([owner, customer]) => {
+    let tether, rwd, decentralBank
+
+    function tokens(number) {
+        return web3.utils.toWei(number, 'ether')
+    }
+
+    before(async () => {
+        // Load contracts
+        tether = await Tether.new()
+        rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        await rwd.transfer(decentralBank.address, tokens('1000000'))
+
+        await tether.transfer(customer, tokens('100'), { from: owner })
+    })
+
+    describe('Mock Tether Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await tether.name()
+            assert.equal(name, 'Mock Tether Token')
+        })
+    })
+
+    describe('Reward Token Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await rwd.name()
+            assert.equal(name, 'Reward Token')
+        })
+    })
+
+    describe('Decentral Bank Deployment', async () => {
+        it('matches name successfully', async () => {
+            const name = await decentralBank.name()
+            assert.equal(name, 'DecentralBank')
+        })
+
+        it('contract has tokens', async () => {
+            let balance = await rwd.balanceOf(decentralBank.address)
+            assert.equal(balance, tokens('1000000'))
+        })
+    })
+})
